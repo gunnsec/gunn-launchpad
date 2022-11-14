95cd1f7524eab551bf261379bbf87decaf7b3c77
Kevin Yu <60120929+ky28059@users.noreply.github.com>
2022-11-14 14:55:58 -0800

    Add IDE background on `/tech`

--- pages/tech.tsx
+++ pages/tech.tsx
@@ -15,39 +15,43 @@ export default function Tech() {
 
             <Header />
 
-            <section className="flex flex-col items-center mb-12">
-                <h1 className="text-7xl font-bold mb-6 text-center">Tech Initiative</h1>
+            <section className="relative flex flex-col items-center -mb-5 z-10">
+                <h1 className="text-7xl font-bold mb-6 text-center">Tech Initiative 🚀{/* 🚀*/}</h1>
                 <button className="font-medium py-2 px-4 rounded bg-gradient-to-br from-orange-500 via-red-500 to-pink-500 shadow-md shadow-red-700 text-white">
                     Register now
                 </button>
             </section>
 
-            <div className="relative container lg:max-w-[58rem]">
-                <MockPage />
+            <div className="relative mx-auto max-w-[90%] xl:container">
+                <img src="/ide.png" alt="IDE" className="w-full opacity-60 rounded-t-lg sm:h-[16rem] md:h-[20rem] lg:h-[24rem] object-cover object-top" />
 
-                <div className="relative bg-white dark:bg-midnight rounded-lg shadow-lg w-96 z-10 mx-auto">
-                    <div className="flex gap-2 p-2 border-b border-gray-100/20">
-                        <div className="w-2 h-2 rounded-full bg-red-500" />
-                        <div className="w-2 h-2 rounded-full bg-yellow-500" />
-                        <div className="w-2 h-2 rounded-full bg-lime-500" />
+                <div className="container lg:max-w-[58rem] absolute z-20 -bottom-32 inset-x-0 mx-auto">
+                    <MockPage />
+
+                    <div className="relative bg-white dark:bg-midnight rounded-lg shadow-lg w-96 z-10 mx-auto">
+                        <div className="flex gap-2 p-2 border-b border-gray-100/20">
+                            <div className="w-2 h-2 rounded-full bg-red-500" />
+                            <div className="w-2 h-2 rounded-full bg-yellow-500" />
+                            <div className="w-2 h-2 rounded-full bg-lime-500" />
+                        </div>
+                        <div className="flex flex-col gap-3 py-3 px-4 font-light">
+                            <div className="h-20 bg-gray-300/40 dark:bg-gray-300/10 rounded-lg animate-pulse" />
+                            <p>
+                                Tech Initiative is a collaboration between SEC and student programmers, creators, and
+                                innovators.
+                            </p>
+                            <p>
+                                Through this partnership, ___.
+                            </p>
+                            <p className="mb-2">See our currently chartered projects <a>here</a>.</p>
+                        </div>
                     </div>
-                    <div className="flex flex-col gap-3 py-3 px-4 font-light">
-                        <div className="h-20 bg-gray-300/40 dark:bg-gray-300/10 rounded-lg animate-pulse" />
-                        <p>
-                            Tech Initiative is a collaboration between SEC and student programmers, creators, and
-                            innovators.
-                        </p>
-                        <p>
-                            Through this partnership, ___.
-                        </p>
-                        <p className="mb-2">See our currently chartered projects <a>here</a>.</p>
-                    </div>
+
+                    <MockPage right />
                 </div>
-
-                <MockPage right />
             </div>
 
-            <section className="-mt-24 pt-36 pb-12 my-4 bg-gray-100 dark:bg-dark">
+            <section className="pt-48 pb-16 bg-gray-100 dark:bg-dark">
                 <div className="container">
                     <ol className="flex flex-col gap-12 pl-20 list-outside list-decimal marker:font-bold marker:text-4xl marker:text-grapefruit">
                         <Benefit name="Publicity">
