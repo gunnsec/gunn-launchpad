42c812fc5799425d30511610e670d2136b8bb916
Kevin Yu <kevinyu567@gmail.com>
2023-04-14 11:48:44 -0700

    Header styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import {ReactNode} from 'react';
 import Link from 'next/link';
+import {useRouter} from 'next/router';
 import {useScroll} from '../util/useScroll';
 import ThemeToggle from './ThemeToggle';
 
@@ -8,7 +9,8 @@ export default function Header() {
     const scroll = useScroll();
 
     return (
-        <header className={'z-40 flex items-center px-4 sticky top-0 mb-28 bg-white/60 dark:bg-midnight/60 transition-shadow duration-200 backdrop-blur ' + (scroll > 0 ? 'shadow-md' : 'hover:shadow-md')}>
+        <header className={'z-40 flex items-center px-6 lg:px-14 sticky top-0 mb-28 bg-white/60 dark:bg-midnight/60 transition-shadow duration-200 backdrop-blur ' + (scroll > 0 ? 'shadow-sm' : 'hover:shadow-md')}>
+            {/* TODO: logo */}
             <HeaderItem href="/">Home</HeaderItem>
             <HeaderItem href="/art">Art</HeaderItem>
             <HeaderItem href="/tech">Tech</HeaderItem>
@@ -19,8 +21,11 @@ export default function Header() {
 }
 
 function HeaderItem(props: {href: string, children: ReactNode}) {
+    const {pathname} = useRouter();
+    const active = pathname === props.href;
+
     return (
-        <Link href={props.href} className="p-4 font-medium text-secondary dark:text-secondary-dark">
+        <Link href={props.href} className={'px-3.5 py-6 font-medium tracking-narrow text-sm ' + (active ? 'text-grapefruit' : 'text-secondary dark:text-secondary-dark')}>
             {props.children}
         </Link>
     )
