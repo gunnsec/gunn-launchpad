08fa0fa3c9deff0f9b18021b724015d53f4a18ac
Kevin Yu <kevinyu567@gmail.com>
2023-04-14 11:37:26 -0700

    Make permalinks jump slightly above heading, add font

--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -4,11 +4,14 @@ import {FiLink} from 'react-icons/fi';
 
 export default function SectionHeader(props: {children: ReactNode, id: string}) {
     return (
-        <a href={`#${props.id}`} className="group flex gap-2 mb-5 items-center hover:underline decoration-1 underline-offset-4 decoration-secondary dark:decoration-secondary-dark decoration-dotted">
-            <h2 className="text-2xl font-bold" id={props.id}>
-                {props.children}
-            </h2>
-            <FiLink className="hidden group-hover:block text-xl pb-0.5 mt-2 stroke-[1.5px]" />
-        </a>
+        <div className="relative">
+            <span id={props.id} className="absolute -top-24" />
+            <a href={`#${props.id}`} className="group flex gap-2 mb-5 items-center hover:underline decoration-1 underline-offset-4 decoration-secondary dark:decoration-secondary-dark decoration-dotted">
+                <h2 className="text-2xl font-bold">
+                    {props.children}
+                </h2>
+                <FiLink className="hidden group-hover:block text-xl pb-0.5 mt-2 stroke-[1.5px]" />
+            </a>
+        </div>
     )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import {AppProps} from 'next/app';
+import {Inter} from 'next/font/google';
 import Header from '../components/Header';
 
 import '../styles/globals.css';
 
 
+const inter = Inter({
+    subsets: ['latin']
+});
+
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <div>
+        <div className={inter.className}>
             <Header />
             <Component {...pageProps} />
         </div>
