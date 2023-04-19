1575384f1f2f594d2bd98bdba0f8b5c6bdaf0583
Kevin Yu <kevinyu567@gmail.com>
2023-04-19 14:53:04 -0700

    Comment out nonexistent pages

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,9 +13,9 @@ export default function Header() {
         <header className={'z-40 w-full flex items-center px-6 lg:px-14 fixed top-0 bg-white/60 dark:bg-midnight/60 transition-shadow duration-200 backdrop-blur ' + (scroll > 0 ? 'shadow-sm' : 'hover:shadow-sm')}>
             {/* TODO: logo */}
             <HeaderItem href="/">Home</HeaderItem>
-            <HeaderItem href="/art">Art</HeaderItem>
+            {/* <HeaderItem href="/art">Art</HeaderItem> */}
             <HeaderItem href="/tech">Tech</HeaderItem>
-            <HeaderItem href="/events">Events</HeaderItem>
+            {/* <HeaderItem href="/events">Events</HeaderItem> */}
             <HeaderItem href="/resources">Resources</HeaderItem>
             <ThemeToggle className="ml-auto mr-4" />
         </header>
