d980ce56551f009f8383fb1240e0b48860b6668a
Ethan Liang <e3lcool@gmail.com>
2023-05-01 22:07:11 -0700

    add link to gunnsec.org to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,9 +5,13 @@ export default function Footer() {
     return (
         <footer className="mt-24 py-16 border-t border-tertiary dark:border-tertiary-dark">
             <div className="container text-secondary dark:text-secondary-dark text-sm flex justify-between">
-                Copyright © 2023. All rights reserved.
+                <p>
+                    Latest information and resources can be found at <a href="https://gunnsec.org/" target="_blank" rel="noopener noreferrer">gunnsec.org</a>.
+                    <br />
+                    Copyright © 2023. All rights reserved.
+                </p>
                 <a href="https://github.com/gunnsec/gunn-launchpad" target="_blank" rel="noopener noreferrer" className="text-base">
-                    <BsGithub />
+                    <BsGithub className="my-auto h-full" />
                 </a>
             </div>
         </footer>
